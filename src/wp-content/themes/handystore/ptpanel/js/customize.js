a7ae44c7c1bc87f48a151ebf5db93d20d90497a2
khanh11166 <khanh11166@gmail.com>
2015-12-02 00:27:22 +0700

    rm

--- src/wp-content/themes/handystore/ptpanel/js/customize.js
+++
@@ -1,8 +0,0 @@
-
-(function(){
-    "use strict";
-jQuery(document).ready(function($) {
-	$("select, input[type=checkbox], input[type=radio], input[type=file]").styler(); 
-	//$("input,select,textarea,radio").not("[type=submit]").jqBootstrapValidation();
-});
-})();
